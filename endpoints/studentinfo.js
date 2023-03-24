52b9aca1db0124e459780caa1ed9f3da0f7278b4
Kaushik Duddala <Kaushik.duddala.964@k12.friscoisd.org>
2023-03-24 04:43:44 +0000

    removed the variable assigns and just put it into the object instead & removed the console.log

--- endpoints/studentinfo.js
+++ endpoints/studentinfo.js
@@ -8,22 +8,14 @@ export async function getStudentInfo(user, pass) {
   let page
   await fetch(cookies, "https://hac.friscoisd.org/HomeAccess/Content/Student/Registration.aspx").then(async (data) => page = await data.text())
   const pageParser = new JSDOM(page)
-  const studentName = await pageParser.window.document.getElementById("plnMain_lblRegStudentName").textContent
-  const studentId = await pageParser.window.document.getElementById("plnMain_lblRegStudentID").textContent
-  const studentBirthday = await pageParser.window.document.getElementById("plnMain_lblBirthDate").textContent
-  const studentCounselor = await pageParser.window.document.getElementById("plnMain_lblCounselor").textContent
-  const studentCounselorEmail = await pageParser.window.document.getElementsByTagName("a")[0].href.match(/mailto:(.*)/)[1]
-  const studentGrade = await pageParser.window.document.getElementById("plnMain_lblGrade").textContent
-  const studentSchool = await pageParser.window.document.getElementById("plnMain_lblBuildingName").textContent
   const pushBack = {
-    "studentName": studentName,
-    "studentId": studentId,
-    "studentBirthday": studentBirthday,
-    "studentCounselor": studentCounselor,
-    "studentCounselorEmail": studentCounselorEmail,
-    "studentGrade": studentGrade,
-    "studentSchool": studentSchool
+    "studentName": pageParser.window.document.getElementById("plnMain_lblRegStudentName").textContent,
+    "studentId": pageParser.window.document.getElementById("plnMain_lblRegStudentID").textContent,
+    "studentBirthday": pageParser.window.document.getElementById("plnMain_lblBirthDate").textContent,
+    "studentCounselor": pageParser.window.document.getElementById("plnMain_lblCounselor").textContent,
+    "studentCounselorEmail": pageParser.window.document.getElementsByTagName("a")[0].href.match(/mailto:(.*)/)[1],
+    "studentGrade": pageParser.window.document.getElementById("plnMain_lblGrade").textContent,
+    "studentSchool": pageParser.window.document.getElementById("plnMain_lblBuildingName").textContent
   }
-  console.log(pushBack)
   return pushBack
 }
