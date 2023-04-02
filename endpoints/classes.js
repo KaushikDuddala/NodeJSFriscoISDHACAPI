98fff208a7d323f2fa5bd24533e0ab0562e8cca4
Kaushik Duddala <Kaushik.duddala.964@k12.friscoisd.org>
2023-04-02 20:45:45 +0000

    Finished Classes/grades endpoint

---
+++ endpoints/classes.js
@@ -0,0 +1,60 @@
+import { fetch, CookieJar } from "node-fetch-cookies";
+import { getSession } from "./../session.js"
+import jsdom from "jsdom"
+const { JSDOM } = jsdom;
+
+export async function getClasses(username, password){
+    let returnDataArray = [];
+    let page
+    const cookies = await getSession(username, password)
+    await fetch(cookies, "https://hac.friscoisd.org/HomeAccess/Content/Student/Assignments.aspx").then(async (data) => page = await data.text())
+    const pageParser = new JSDOM(page)
+    const classesDiv = Object.values(pageParser.window.document.getElementsByClassName("AssignmentClass"))
+    classesDiv.forEach(async (classDiv) => {
+        let className;
+        let lastUpdated;
+        let grade;
+        let assignments = [];
+        const headerDivs = Object.values(classDiv.getElementsByClassName("sg-header sg-header-square"))
+        headerDivs.forEach(async (headerDiv) => {
+            className = headerDiv.getElementsByClassName("sg-header-heading")[0].textContent.trim()
+            lastUpdated = headerDiv.getElementsByClassName("sg-header-sub-heading")[0].textContent.trim().replace("(Last Updated: ", "").replace(")","")
+            grade = headerDiv.getElementsByClassName("sg-header-heading sg-right")[0].textContent.trim().replace("Student Grades ", "").replace("%", "")
+            if(grade == ""){
+                grade = "0.00"
+            }
+            if(lastUpdated == ""){
+                lastUpdated = "n/a"
+            }
+        })
+        let assignmentsArray = []
+        const assignmentsDiv = Object.values(classDiv.getElementsByClassName("sg-content-grid"))
+        assignmentsDiv.forEach(async (assignmentDiv) => {
+            const assignments = Object.values(assignmentDiv.getElementsByClassName("sg-asp-table-data-row"))
+            assignments.forEach(async (assignment) => {
+                try{
+                let grade = assignment.getElementsByTagName("td")[4].textContent.replace("\n", "").trim()
+                if (grade == ""){
+                    grade = "0.00"
+                }
+                const assignmentData =  {
+                    "assignmentName": assignment.getElementsByTagName("a")[0].textContent.replace("\n", "").trim(),
+                    "assignmentType": assignment.getElementsByTagName("td")[3].textContent,
+                    "assignmentGrade": grade,
+                    "assignmentDueDate": assignment.getElementsByTagName("td")[0].textContent,
+                    "assignmentAssignedDate": assignment.getElementsByTagName("td")[1].textContent.trim(),
+                    "assignmentMaxScore": assignment.getElementsByTagName("td")[5].textContent.
+                }
+                assignmentsArray.push(assignmentData)
+            }catch(error){}})
+        })
+        let courseData = {
+            "className": className,
+            "grade": grade,
+            "lastUpdated": lastUpdated,
+            "assignments":assignmentsArray
+        }
+        returnDataArray.push(courseData)
+    })
+    return returnDataArray
+}
--- index.js
+++ index.js
@@ -2,9 +2,11 @@ import express from 'express'
 const app = express()
 import { getSchedule } from "./endpoints/schedule.js"
 import { getStudentInfo } from "./endpoints/studentinfo.js"
+import { getClasses } from './endpoints/classes.js'
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
 
 async function validateUserAndPass(reqBody){
   let user
@@ -21,9 +23,12 @@ async function validateUserAndPass(reqBody){
   return [user, pass]
 }
 
+
 app.get('/', function(req, res) {
   res.send('{\'message\':\'Welcome to the HAC API\'}')
 })
+
+
 app.post('/schedule', async function(req, res) {
   const loginDetails = await validateUserAndPass(req.body)
   try {
@@ -44,6 +49,20 @@ app.post('/info', async function(req, res) {
   const loginDetails = await validateUserAndPass(req.body)
   try {
     const response = await getStudentInfo(loginDetails[0], loginDetails[1])
+    res.send(response)
+    res.status(200)
+    res.end()
+  } catch (error) {
+    res.send(`{'error':"${error}"`)
+    res.status(401)
+    res.end()
+  }
+})
+
+app.post('/classes', async function(req, res) {
+  const loginDetails = await validateUserAndPass(req.body)
+  try {
+    const response = await getClasses(loginDetails[0], loginDetails[1])
     res.send(response)
     res.status(200)
     res.end()
